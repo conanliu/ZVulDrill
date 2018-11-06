8250718682fba4cdcea11eefed7ed45a0b5440d2
conanliu <conanliu@m.scnu.edu.cn>
2018-11-06 21:10:23 +0800

    Update config.php

--- sys/config.php
+++ sys/config.php
@@ -1,8 +1,8 @@
 <?php
 //链接数据库
-$host = '134.175.80.128';
-$username = 'root';
-$password = '123456778';
+$host = '134.175.80.199';
+$username = 'test@tencent.com';
+$password = 'tenCent.tt';
 $port = '3306';
 $database = 'zvuldrill';
 $dbc = mysqli_connect($host, $username, $password, $database);
